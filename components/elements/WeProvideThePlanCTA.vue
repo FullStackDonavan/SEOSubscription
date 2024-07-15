9959ce5c63804038d07665e310c7d422af3e5422
DonavanJones <donavancjones@gmail.com>
2024-07-15 16:11:35 -0400

    first commit

--- components/elements/WeProvideThePlanCTA.vue
+++ components/elements/WeProvideThePlanCTA.vue
@@ -3,8 +3,8 @@
     id="ServiceArea"
     class="mx-auto p-8 bg-blue-400 text-gray-300 dark:bg-amber-400 dark:text-gray-300 text-center map-image flex flex-col justify-center items-center relative dark:bg-indigo-600 text-gray-800"
   >
-    <PaintDripTop fill="60A5FA" v-if="$colorMode.value == 'light'" />
-    <PaintDripTop fill="4F46E5" v-if="$colorMode.value == 'dark'" />
+    <PaintDripTop fill="60A5FA" v-if="$colorMode.value === 'light'" />
+    <PaintDripTop fill="4F46E5" v-else />
     <p class="mb-4 font-extrabold tracking-tight leading-none text-white">
       All From A Company That Exclusively Provides Marketing For Contractors
     </p>
@@ -14,10 +14,11 @@
       We Provide The Plan / To Get You The Leads /
       <strong>That Keeps Your Schedule Full</strong>
     </h3>
-    <PaintDripBottom fill="60A5FA" v-if="$colorMode.value == 'light'" />
-    <PaintDripBottom fill="4F46E5" v-if="$colorMode.value == 'dark'" />
+    <PaintDripBottom fill="60A5FA" v-if="$colorMode.value === 'light'" />
+    <PaintDripBottom fill="4F46E5" v-else />
   </div>
 </template>
+
 <script>
 export default {
   name: "WeProvideThePlanCTA",
@@ -26,7 +27,11 @@ export default {
       colorMode: useColorMode(),
     };
   },
+  mounted() {
+    console.log("Current colorMode:", this.colorMode.value);
+  },
 };
 </script>
+
 
 
